d3c3279279522d61c4b78df6824c03462bb72cb9
Carmen Johnson <carmenjohnson512@gmail.com>
2020-07-02 18:52:51 -0500

    created Employee, Manager and Engineer classes

--- 10-EE-Summary-Template-Engine/Develop/lib/Employee.js
+++ 10-EE-Summary-Template-Engine/Develop/lib/Employee.js
@@ -1,1 +1,30 @@
-// TODO: Write code to define and export the Employee class
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+    getName(name) {
+        console.log(name);
+    };
+    getID(id) {
+        console.log(id);
+    };
+    getEmail(email) {
+        console.log(email);
+    };
+    getRole() {
+        let role = "Employee";
+        return(role)
+    };
+    
+};
+
+const employee1 = new Employee("Carmen", "cj12345", "email@someemail.com");
+employee1.getName();
+employee1.getID();
+employee1.getEmail();
+employee1.getRole();
+
+
+module.exports = Employee;
--- 10-EE-Summary-Template-Engine/Develop/lib/Engineer.js
+++ 10-EE-Summary-Template-Engine/Develop/lib/Engineer.js
@@ -1,1 +1,29 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+// TODO: Write code to define and export the Engineer class.  
+//HINT: This class should inherit from Employee.
+
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    constructor(name, id, email, github) {
+        super({ name, id, email });
+        this.github = github;
+    }
+
+    getGithub() {
+        console.log(this.github); 
+    }
+
+    getRole() {
+        let role = "Engineer";
+        return(role)
+    }
+}
+
+const engineer1 = new Engineer("Carmen", "cj12345", "email@someemail.com", "carmenjohnson512");
+engineer1.getName();
+engineer1.getID();
+engineer1.getEmail();
+engineer1.getGithub();
+engineer1.getRole();
+
+module.exports = Engineer;
