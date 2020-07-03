a88d9cfa391c72a7218b4fe057b431dfae7eb45d
Carmen Johnson <carmenjohnson512@gmail.com>
2020-07-02 20:22:05 -0500

    Engineer class is successfully inheriting Employee class properties

--- 10-EE-Summary-Template-Engine/Develop/lib/Engineer.js
+++ 10-EE-Summary-Template-Engine/Develop/lib/Engineer.js
@@ -1,12 +1,13 @@
 // TODO: Write code to define and export the Engineer class.  
 //HINT: This class should inherit from Employee.
 
-const Employee = require("./Employee");
+const Employee = require("./employee");
 
 class Engineer extends Employee {
-    constructor(name, id, email, github) {
-        super({ name, id, email });
+    constructor(name, id, email, github, role) {
+        super(name, id, email);
         this.github = github;
+        this.role = role;
     }
 
     getGithub() {
@@ -14,16 +15,15 @@ class Engineer extends Employee {
     }
 
     getRole() {
-        let role = "Engineer";
-        return(role)
+        console.log(this.role);
     }
 }
 
-const engineer1 = new Engineer("Carmen", "cj12345", "email@someemail.com", "carmenjohnson512");
-engineer1.getName();
-engineer1.getID();
-engineer1.getEmail();
-engineer1.getGithub();
-engineer1.getRole();
+const engineer1 = new Engineer("Carmen", "cj12345", "email@someemail.com", "carmenjohnson512", "Engineer");
+engineer1.getName("Carmen");
+engineer1.getID("cj12345");
+engineer1.getEmail("email@someemail.com");
+engineer1.getGithub("carmenjohnson512");
+engineer1.getRole("Engineer");
 
 module.exports = Engineer;
